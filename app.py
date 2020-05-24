d9ea98facf47270189fd38a59f649e62b59df87d
soroushh <soroush@tails.com>
2020-05-24 17:05:07 +0100

    The bootstrap is added to our application and we can use the different classes of it.

--- app.py
+++ app.py
@@ -17,6 +17,12 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/about')
+def about():
+    """."""
+    return render_template('about.html')
+
+
 @app.route('/create-cat/<name>/<breed>')
 def create_cat(name, breed):
     """."""
